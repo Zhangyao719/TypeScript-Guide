5b01322a6c9f180c0e82ff13637a747c30283e95
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-03-20 22:19:30 +0800

    feat(function): 调用签名

--- src/function/function.ts
+++ src/function/function.ts
@@ -1,4 +1,3 @@
-namespace Function1 {
 /*   let triangle = {
     a: 10,
     b: 15,
@@ -22,19 +21,28 @@ namespace Function1 {
     area(this: Triangle): () => number;
   }
   
-  // let triangle: Triangle = {
-  //   a: 10,
-  //   b: 15,
-  //   c: 20,
-  //   area: function (this: Triangle) {
-  //     return () => {
-  //       const p = (this.a + this.b + this.c) / 2
-  //       return Math.sqrt(p * (p - this.a) * (p - this.b) *(p - this.c))
-  //     }
-  //   }
-  // }
+  let triangle: Triangle = {
+    a: 10,
+    b: 15,
+    c: 20,
+    area: function (this: Triangle) {
+      return () => {
+        const p = (this.a + this.b + this.c) / 2
+        return Math.sqrt(p * (p - this.a) * (p - this.b) *(p - this.c))
+      }
+    }
+  }
   
-  // const myArea = triangle.area()
-  // console.log(myArea())
+  const myArea = triangle.area()
+  console.log(myArea())
 
-}
+  /**
+   * 1. 简写型 调用签名
+   * type Log = (msg: string, id?: string) => void;
+   * 
+   * 2. 完整型 调用签名 (写多个 就是函数重载)
+   * type Log = {
+   *  (msg: string, id: string) => void;
+   *  (msg: string, id: number) => void;
+   * }
+  */
