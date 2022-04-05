3ae38fb62477248cd087bbc81df7683bf69e0649
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-04-05 23:38:03 +0800

    feat: what is convariant ?

--- src/function/covariance.ts
+++ src/function/covariance.ts
@@ -8,6 +8,11 @@ type ExistingUser = {
 
 type NewUser = Pick<ExistingUser, 'name'>
 
+type LegacyUser = {
+  id?: number | string;
+  name: string;
+}
+
 function deleteUser(user: { id?: number; name: string }) {
   delete user.id;
   return user;
@@ -23,9 +28,22 @@ deleteUser(existingUser);
 existingUser.id // 显示id为number 但其实已经删掉了 是 undefined
 
 // ? 安全问题:
-// ? 1. ts 不会报错 因为 ExistingUser 是 { id?: number; name: string } 的子类型
+// ? 1. ts 不会报错 因为 id: number 是 id?: number 的子类型
 // ? 2. 删除之后 依旧显示为 number 类型
 // ? 由于破坏性更新(比如删除一个属性)在实际中很少见, 所以ts放款了要求
 
+let legacyUser: LegacyUser = {
+  id: '102',
+  name: 'James',
+}
 
+deleteUser(legacyUser); // error string 不能赋值给 number | undefined
+
+// ? 原因: 超类型不能 传递给 子类型
+
+/**
+ * * 总结:
+ * * 在类型上, TS 对结构(对象和类)的属性进行了协变(covariant)
+ * * 也就是说, 如果想保证A对象可以赋值给B对象, 那么A对象的每个属性都必须是B对象对应属性的子类型
+ */
 
