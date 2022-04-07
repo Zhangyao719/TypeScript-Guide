9f3adc2c2d4e401ffca96c7ef3f5ae044b694ae6
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-04-08 00:34:18 +0800

    feat: extends infer

--- src/advanced-type/extends.ts
+++ src/advanced-type/extends.ts
@@ -1,14 +1,26 @@
-interface Prop {
-  propA: boolean;
-  propB: boolean;
-}
+// 有条件类型
 
-declare function f<T>(x: T): T extends Prop ? string : number;
+// * 分布式条件类型 过滤 联合类型
+type Without<T, U> = T extends U ? never : T
 
-function foo<U>(x: U) {
-  // Has type 'U extends Prop ? string : number'
-  let a = f(x);
+type P = Without<boolean | string | number, boolean> // string | number
 
-  // This assignment is allowed though!
-  let b: string | number = a;
-}
+/**
+ * * infer 关键字
+ * * 只能在条件语句中使用
+ * * 用来声明泛型
+ * * 靠ts自动推导
+ */
+// 例一: 获取数组中元素的类型
+type ElementType<T> = T extends unknown[] ? T[number] : T
+type O1 = ElementType<number[]>
+type O2 = ElementType<(number | string)[]>
+// T[number] 数组的键入 用来获取数组元素类型
+
+// * 可以重写成:
+type ElementType1<T> = T extends (infer U)[] ? U : T
+
+// 例二: 获取函数参数的类型
+type SecondArg<F> = F extends (a: any, b: infer B) => any ? B : never
+type F = typeof Array['prototype']['slice']
+type I = SecondArg<F>
